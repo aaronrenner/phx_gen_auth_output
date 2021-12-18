b6ce29743f8e07bee0bc20b3d68a5190505dc070
Aaron Renner <aaron.renner@gmail.com>
2021-12-17 22:01:21 -0700

    Output from v1.6.1

--- mix.exs
+++ mix.exs
@@ -33,7 +33,8 @@ defmodule Demo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "1.6.0"},
+      {:bcrypt_elixir, "~> 2.0"},
+      {:phoenix, "1.6.1"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
